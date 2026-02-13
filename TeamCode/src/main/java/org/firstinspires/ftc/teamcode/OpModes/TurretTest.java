374f9592faa3a7c17e0de03a72bd076ed29a898a
kyler <kylerobertmalt@gmail.com>
2026-02-13 15:15:05 -0500

    TurretTest rollback fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TurretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TurretTest.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
 import static org.firstinspires.ftc.teamcode.Config.Localization.LimelightFiducial.TX;
+import static org.firstinspires.ftc.teamcode.Config.RobotConstants.kP;
+import static org.firstinspires.ftc.teamcode.Config.RobotConstants.kI;
+import static org.firstinspires.ftc.teamcode.Config.RobotConstants.kD;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.follower.Follower;
@@ -9,8 +12,8 @@ import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.arcrobotics.ftclib.controller.PIDController;
 
 import org.firstinspires.ftc.teamcode.Config.Localization.LimelightFiducial;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
@@ -23,15 +26,10 @@ public class TurretTest extends OpMode {
     private Limelight3A limelight;
     private LimelightFiducial LimeInit;
     private DcMotorEx TurretMotor;
-    private PIDController pid;
-    private double turretPower;
-    public static double kP = 0.3;
-    public static double kI = 0.00;
-    public static double kD = 0.0001;
-    public static double DeadDeg = 1.5;
     private double setpoint = 0;
     private Follower follower;
     private Pose startingPose;
+    private List<LLResultTypes.FiducialResult> fiducials;
 
     @Override
     public void init() {
@@ -42,14 +40,20 @@ public class TurretTest extends OpMode {
         TurretMotor = hardwareMap.get(DcMotorEx.class, "TurretMotor");
 
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-        limelight.pipelineSwitch(1);
+        limelight.pipelineSwitch(1); //1 blue 0 red
         LimeInit = new LimelightFiducial();
         LimeInit.LimelightInit(limelight);
-        pid = new PIDController(kP, kI, kD);
 
+        TurretMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         TurretMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         TurretMotor.setDirection(DcMotorEx.Direction.FORWARD);
         TurretMotor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    }
+
+    @Override
+    public void init_loop(){
+        telemetry.addData("Turret Position",TurretMotor.getCurrentPosition());
+        telemetry.update();
     }
 
     @Override
@@ -65,32 +69,12 @@ public class TurretTest extends OpMode {
 
     @Override
     public void loop(){
-        LimeInit.getLimePose();
+        double turretPower = LimeInit.getLimePose();
         follower.update();
 
-        com.qualcomm.hardware.limelightvision.LLResult result = limelight.getLatestResult();
-        if (result == null || !result.isValid()) {
-            TurretMotor.setPower(gamepad2.left_stick_x);
-            return;
-        }
-        List<LLResultTypes.FiducialResult> fiducials = result.getFiducialResults();
-        if (fiducials.isEmpty()) {
-            TurretMotor.setPower(gamepad2.left_stick_x);
-            return;
-        }
-        double tx = fiducials.get(0).getTargetXDegrees();
-        turretPower = pid.calculate(tx);
-        turretPower = Math.max(-0.6, Math.min(0.6, turretPower));
-        //turretPower *= Math.min(1, Math.abs(tx) / deadband); //Output taper
         TurretMotor.setPower(turretPower);
 
-        //Stop if within deadband
-        if (Math.abs(tx) < DeadDeg) {
-            TurretMotor.setPower(0);
-            pid.reset();
-        }
-
-        telemetry.addData("TX in degrees", tx);
+        telemetry.addData("TX in degrees", TX);
         telemetry.addData("kP Proportional", kP);
         telemetry.addData("kI Integral", kI);
         telemetry.addData("kD Derivative", kD);
